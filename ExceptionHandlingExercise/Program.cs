d20403979350442e822640d6c2b3d5f8e209074b
Cam Kellett <cannicam@gmail.com>
2020-12-02 15:32:45 -0600

    finished exercise

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -12,37 +12,47 @@ namespace ExceptionHandlingExercise
             // First create an char[], it must contain 6 numbers and 3 letters - name it arr
             // Create a list called numbers that will hold integers
             // Create an string variable with an empty string initializer - name it str
-
+            char[] arr = {'2','4','6','7','8','9','A','M','Z'};
+            var numbers = new List<int>();
+            string str = string.Empty;
             // using a foreach loop, attempt to parse the elements in your char[] with int.Parse()
             // and Exceptions will be thrown 
             // Below we will set this up 
             // ------------------------------------------------------------------------------
 
-
-
             //TODO START HERE:
-            
+
             // Make a foreach loop to iterate through your character array
-            
-                // Now create a try catch
-                
+            foreach (var c in arr)
+            {
+            // Now create a try catch
                 
                     // Inside your try block
                         // set your string variable to each array element in your char[] to .ToString()
                         // Now, using int.Parse, parse your string variable and store in an int variable
                         // Then add each int to your list
+                try
+                {
+                    str = c.ToString();
+                    int num = int.Parse(str);
+                    numbers.Add(num);
+                }
                 
                 // catch your Exception:
                 // in the scope of your catch you can use the following, 
                 
                     //Console.WriteLine($"Unable to Parse '{character}'"); //character will be the name of each item in your collection
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Unable to Parse '{c}'");
+                    Console.WriteLine(ex.Message); 
+                }
+            }
                 
-            
-
-            //foreach (var num in numbers)
-            //{
-            //    Console.WriteLine(num);
-            //}
+           foreach (var num in numbers)
+           {
+               Console.WriteLine(num);
+           }
         }
     }
 }
